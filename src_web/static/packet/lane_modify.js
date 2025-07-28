dc32aa1e6272cba882d0c413f41afa52e7368739
aKuad <53811809+aKuad@users.noreply.github.com>
2025-07-28 22:24:22 +0900

    Fix: Unify to strict comparison operator
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src_web/static/packet/lane_modify.js
+++ src_web/static/packet/lane_modify.js
@@ -86,7 +86,7 @@ export function decode_lane_modify_packet(packet) {
  * @returns {boolean} `packet` is lane-modify packet: true, otherwise: false
  */
 export function is_lane_modify_packet(packet) {
-  if(packet.byteLength != 4) return false;
+  if(packet.byteLength !== 4) return false;
 
   const packet_id = new DataView(packet).getUint8(0);
   return packet_id === PACKET_ID_LANE_MODIFY;
