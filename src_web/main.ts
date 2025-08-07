d6c4053913b03d0d4ff924913ae977d2e3123d97
aKuad <53811809+aKuad@users.noreply.github.com>
2025-08-07 22:44:45 +0900

    Add: Lane-modify packet received monitoring on server
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src_web/main.ts
+++ src_web/main.ts
@@ -4,6 +4,8 @@
 
 import { serveDir, serveFile } from "jsr:@std/http@1";
 import { DMX_CHANNEL_COUNT, encode_lanes_initialize_packet } from "./static/packet/lanes_initialize.js";
+import { decode_lane_modify_packet, is_lane_modify_packet } from "./static/packet/lane_modify.js";
+
 
 const dmx_values = new Uint8Array(DMX_CHANNEL_COUNT);
 dmx_values[1] = 255;  // Channel 2 set 255
@@ -27,8 +29,14 @@ Deno.serve(request => {
       return new Response("This API is for websocket, protocol upgrade required", { status: 426 });
 
     const { response, socket } = Deno.upgradeWebSocket(request);
+    // Apply current values to client UI
     socket.addEventListener("open", () => {
       socket.send(encode_lanes_initialize_packet(dmx_values));
+    });
+    // On lane controlled by client
+    socket.addEventListener("message", e => {
+      if(is_lane_modify_packet(e.data))
+        console.log(decode_lane_modify_packet(e.data));
     });
     return response;
   }
