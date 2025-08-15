46655051b0a0ca21913e48045671c0787faa18d6
aKuad <53811809+aKuad@users.noreply.github.com>
2025-08-15 10:05:14 +0900

    Mod: `lane_modify` packet broadcasting to all clients
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src_web/main.ts
+++ src_web/main.ts
@@ -3,11 +3,14 @@
  */
 
 import { serveDir, serveFile } from "jsr:@std/http@1";
+
+import { ws_broadcast } from "./modules/ws_broadcast.ts";
 import { DMX_CHANNEL_COUNT, encode_lanes_initialize_packet } from "./static/packet/lanes_initialize.js";
 import { decode_lane_modify_packet, is_lane_modify_packet } from "./static/packet/lane_modify.js";
 
 
 const dmx_values = new Uint8Array(DMX_CHANNEL_COUNT);
+const ws_clients = new Set<WebSocket>();
 
 
 Deno.serve(request => {
@@ -27,17 +30,25 @@ Deno.serve(request => {
       return new Response("This API is for websocket, protocol upgrade required", { status: 426 });
 
     const { response, socket } = Deno.upgradeWebSocket(request);
-    // Apply current values to client UI
+    socket.binaryType = "arraybuffer";
+
+    // On a client connected
     socket.addEventListener("open", () => {
       socket.send(encode_lanes_initialize_packet(dmx_values));
+      ws_clients.add(socket);
     });
-    // On lane controlled by client
+    // On lane controlled by a client
     socket.addEventListener("message", e => {
       if(is_lane_modify_packet(e.data)) {
+        ws_broadcast(ws_clients, e.data, socket);
         const { channel, value } = decode_lane_modify_packet(e.data);
         dmx_values[channel-1] = value;
       }
     });
+    // On a client disconnected
+    socket.addEventListener("close", () => ws_clients.delete(socket));
+    socket.addEventListener("error", () => ws_clients.delete(socket));
+
     return response;
   }
 
