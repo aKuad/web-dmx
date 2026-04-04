1ed409a5109c420e2203289f26ae76a673758713
aKuad <53811809+aKuad@users.noreply.github.com>
2026-04-04 22:53:47 +0900

    Add: WebSocket echo endpoint at test HTTP server
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src_web/tests/manual/main.ts
+++ src_web/tests/manual/main.ts
@@ -11,6 +11,21 @@ Deno.serve(request => {
   const url = new URL(request.url);
 
 
+  /* API endpoints */
+  if(url.pathname.startsWith("/api/websocket-echo")) {
+    if(request.headers.get("upgrade") === "websocket") {
+      const { socket, response } = Deno.upgradeWebSocket(request);
+      socket.addEventListener("message", e => {
+        socket.send(e.data);
+      });
+      return response;
+
+    } else {
+      new Response("This API is for websocket, protocol upgrade required", { status: 426 });
+    }
+  }
+
+
   /* Page endpoints */
   if(url.pathname.startsWith("/static")) {
     return serveDir(request, { fsRoot: "../../static", urlRoot: "static"});
