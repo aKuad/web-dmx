60e08447a2f975f8327139019f5734b16dc6c36f
aKuad <53811809+aKuad@users.noreply.github.com>
2026-04-15 22:20:19 +0900

    Mod: Listener setting of `WebSocketAutoRecon` compatibled with `WebSocket`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src_web/static/WebSocketAutoRecon.js
+++ src_web/static/WebSocketAutoRecon.js
@@ -22,30 +22,30 @@ export class WebSocketAutoRecon {
   #reconnect_ms;
 
   /**
-   * Listener function of WebSocket event 'open'
+   * Listener functions of WebSocket event 'open'
    *
-   * @type {function}
+   * @type {function[]}
    */
   #on_open_listener;
 
   /**
-   * Listener function of WebSocket event 'message'
+   * Listener functions of WebSocket event 'message'
    *
-   * @type {function}
+   * @type {function[]}
    */
   #on_message_listener;
 
   /**
-   * Listener function of WebSocket event 'close'
+   * Listener functions of WebSocket event 'close'
    *
-   * @type {function}
+   * @type {function[]}
    */
   #on_close_listener;
 
   /**
-   * Listener function of WebSocket event 'error'
+   * Listener functions of WebSocket event 'error'
    *
-   * @type {function}
+   * @type {function[]}
    */
   #on_error_listener;
 
@@ -60,10 +60,10 @@ export class WebSocketAutoRecon {
     this.#ws = new WebSocket(url);
     this.#reconnect_ms = reconnect_ms;
 
-    this.#on_open_listener    = () => {};
-    this.#on_message_listener = () => {};
-    this.#on_close_listener   = () => {};
-    this.#on_error_listener   = () => {};
+    this.#on_open_listener    = [];
+    this.#on_message_listener = [];
+    this.#on_close_listener   = [];
+    this.#on_error_listener   = [];
 
     this.#ws.addEventListener("close", this.#reconnect.bind(this));
     this.#ws.addEventListener("error", this.#reconnect.bind(this));
@@ -82,32 +82,45 @@ export class WebSocketAutoRecon {
   /**
    * Compatible method of `WebSocket.send()`
    *
-   * @param {string | ArrayBufferLike | Blob | ArrayBufferView} data 
+   * @param {string | ArrayBufferLike | Blob | ArrayBufferView} data Data content to send
    */
   send(data) {
     this.#ws.send(data);
   }
 
 
-  set on_open(listener) {
-    this.#ws.removeEventListener("open", this.#on_open_listener);
-    this.#on_open_listener = listener;
-    this.#ws.addEventListener("open", this.#on_open_listener);
-  }
-  set on_message(listener) {
-    this.#ws.removeEventListener("message", this.#on_message_listener);
-    this.#on_message_listener = listener;
-    this.#ws.addEventListener("message", this.#on_message_listener);
-  }
-  set on_close(listener) {
-    this.#ws.removeEventListener("close", this.#on_close_listener);
-    this.#on_close_listener = listener;
-    this.#ws.addEventListener("close", this.#on_close_listener);
-  }
-  set on_error(listener) {
-    this.#ws.removeEventListener("error", this.#on_error_listener);
-    this.#on_error_listener = listener;
-    this.#ws.addEventListener("error", this.#on_error_listener);
+  /**
+   * Compatible method of `WebSocket.addEventListener()`
+   *
+   * @param {"open" | "message" | "close" | "error"} type Type of event
+   * @param {function} listener Function to call on event dispatch
+   */
+  addEventListener(type, listener) {
+    switch(type) {
+      case "open":
+        this.#ws.addEventListener("open", listener);
+        this.#on_open_listener.push(listener);
+        break;
+
+      case "message":
+        this.#ws.addEventListener("message", listener);
+        this.#on_message_listener.push(listener);
+        break;
+
+      case "close":
+        this.#ws.addEventListener("close", listener);
+        this.#on_close_listener.push(listener);
+        break;
+
+      case "error":
+        this.#ws.addEventListener("error", listener);
+        this.#on_error_listener.push(listener);
+        break;
+
+      default:
+        // Undefined even, do nothing
+        break;
+    }
   }
 
 
@@ -121,13 +134,14 @@ export class WebSocketAutoRecon {
     // WebSocket re-instantiate with same URL
     const url = this.#ws.url;
     const binaryType = this.#ws.binaryType;
-    this.#ws = new WebSocket(url);
+    const ws = new WebSocket(url);
+    this.#ws = ws;
     this.#ws.binaryType = binaryType;
 
-    this.#ws.addEventListener("open"   , this.#on_open_listener);
-    this.#ws.addEventListener("message", this.#on_message_listener);
-    this.#ws.addEventListener("close"  , this.#on_close_listener);
-    this.#ws.addEventListener("error"  , this.#on_error_listener);
+    this.#on_open_listener   .forEach(f => ws.addEventListener("open"   , f));
+    this.#on_message_listener.forEach(f => ws.addEventListener("message", f));
+    this.#on_close_listener  .forEach(f => ws.addEventListener("close"  , f));
+    this.#on_error_listener  .forEach(f => ws.addEventListener("error"  , f));
 
     this.#ws.addEventListener("close", this.#reconnect.bind(this));
     this.#ws.addEventListener("error", this.#reconnect.bind(this));
